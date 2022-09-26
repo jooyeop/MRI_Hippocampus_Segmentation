8b8ead7f1e598d65d293077b741904ab6eabbc26
jooyeop <royalmansu@gmail.com>
2022-09-26 09:22:39 +0900

    Hippocampus

--- Hippocampus.py
+++ Hippocampus.py
@@ -1,7 +1,82 @@
-from mpl_toolkits.mplot3d import Axes3D
+#GENERAL
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import random
+import time
+#PATH PROCESS
+import os
+import os.path
+from pathlib import Path
+import glob
+from scipy.io import loadmat
+import nibabel as nib
+import csv
+#IMAGE PROCESS
+from PIL import Image
+from keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import cv2
+from keras.applications.vgg16 import preprocess_input, decode_predictions
+from keras.preprocessing import image
+from skimage.feature import hessian_matrix, hessian_matrix_eigvals
+from scipy.ndimage.filters import convolve
+from skimage import data, io, filters
+import skimage
+from skimage.morphology import convex_hull_image, erosion
+from IPython import display
+from scipy.ndimage import gaussian_filter
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import matplotlib.patches as patches
+#SCALER & TRANSFORMATION
 from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt # plotting
-import numpy as np # linear algebra
-import os # accessing directory structure
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.preprocessing import MinMaxScaler
+from keras.utils.np_utils import to_categorical
+from sklearn.model_selection import train_test_split
+from keras import regularizers
+from sklearn.preprocessing import LabelEncoder
+#ACCURACY CONTROL
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, roc_auc_score, roc_curve
+from sklearn.model_selection import GridSearchCV, cross_val_score
+from sklearn.metrics import mean_squared_error, r2_score
+#OPTIMIZER
+from keras.optimizers import RMSprop,Adam,Optimizer,Optimizer, SGD
+#MODEL LAYERS
+from tensorflow.keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, BatchNormalization,MaxPooling2D,BatchNormalization,\
+                        Permute, TimeDistributed, Bidirectional,GRU, SimpleRNN,\
+LSTM, GlobalAveragePooling2D, SeparableConv2D, ZeroPadding2D, Convolution2D, ZeroPadding2D,Reshape, Conv2DTranspose,\
+LeakyReLU, GaussianNoise, GlobalMaxPooling2D, ReLU, Input, Concatenate
+from keras import models
+from keras import layers
+import tensorflow as tf
+from keras.applications import VGG16,VGG19,inception_v3
+from keras import backend as K
+from keras.utils import plot_model
+from keras.datasets import mnist
+import keras
+from keras.models import Model
+#IGNORING WARNINGS
+from warnings import filterwarnings
+filterwarnings("ignore",category=DeprecationWarning)
+filterwarnings("ignore", category=FutureWarning) 
+filterwarnings("ignore", category=UserWarning)
+
+#PATH
+Label = Path(r'../dataset/label/35label')
+Image = Path(r'../dataset/original/35')
+
+#IMAGE Path
+Label_Path = list(Label.glob(r'**/*.jpg')) # glob : 파일을 찾아주는 함수
+Image_Path = list(Image.glob(r'**/*.jpg'))
+
+#print('Label_Path : ', len(Label_Path))
+#print('Image_Path : ', len(Image_Path))
+
+Label_Series = pd.Series(Label_Path, name='Label').astype(str) # Series : 1차원 배열
+Image_Series = pd.Series(Image_Path, name='Image').astype(str)
+#print('Label_Series : ', len(Label_Series))
+#print('Image_Series : ', len(Image_Series))
 
